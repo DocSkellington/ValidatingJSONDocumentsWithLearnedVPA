657d853e5399b375e963990092c2dce11791f375
Gaëtan Staquet <gaetan.staquet@gmail.com>
2022-05-31 16:00:27 +0200

    Refactorisation of the two libraries into one

--- permutationAutomaton/src/main/java/be/ac/umons/learningjson/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Implementation of an automaton with permutation for JSON documents.
- * 
- * @author Gaëtan Staquet
- */
-package be.ac.umons.learningjson;
---
+++ validationByAutomaton/src/main/java/be/ac/umons/learningjson/validation/ValidationState.java
@@ -0,0 +1,39 @@
+package be.ac.umons.learningjson.validation;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import net.automatalib.automata.vpda.Location;
+
+/**
+ * A state used in a {@link ValidationByAutomaton}.
+ * 
+ * It contains a set with the source and reached locations in the VPA and a stack.
+ * 
+ * @author Gaëtan Staquet
+ */
+class ValidationState {
+    private final Set<PairSourceToReached> sourceToReachedLocations;
+    private final ValidationStackContents stack;
+
+    public ValidationState(final Set<PairSourceToReached> sourceToReachedLocations, final ValidationStackContents stack) {
+        this.sourceToReachedLocations = sourceToReachedLocations;
+        this.stack = stack;
+    }
+
+    public Set<PairSourceToReached> getSourceToReachedLocations() {
+        return sourceToReachedLocations;
+    }
+
+    public Set<Location> getReachedLocations() {
+        // @formatter:off
+        return sourceToReachedLocations.stream()
+            .map(pair -> pair.getReachedLocation())
+            .collect(Collectors.toSet());
+        // @formatter:on
+    }
+
+    public ValidationStackContents getStack() {
+        return stack;
+    }
+}
